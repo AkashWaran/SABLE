896c24282f1ae235f8a2aeeeb69dfdc119798191
Akash <akash.waran@gmail.com>
2014-10-27 17:45:49 -0400

    Updated functions to take value parameter instead of it being hardcoded

--- Files/buf_of.c
+++ Files/buf_of.c
@@ -1,20 +1,20 @@
-void write_char(void *buf) {
-    unsigned char *ch = buf;
-    *ch = 0x48;
+void write_char(void *buf, int val) {
+    unsigned char *dest = buf;
+    *dest = (unsigned char) val;
 }
 
-void fill_buf(void *buf, unsigned int length) {
-    unsigned char *b = buf;
+void fill_buf(void *buf, unsigned int length, int val) {
+    unsigned char *dest = buf;
     unsigned int i;
     for (i = 0; i < length; i++) {
-	b[i] = 0x69;
+	dest[i] = (unsigned char) val;
     }
 }
 
-void fill_buf_WRONG(void *buf, unsigned int length) {
-    unsigned char *b = buf;
+void fill_buf_WRONG(void *buf, unsigned int length, int val) {
+    unsigned char *dest = buf;
     unsigned int i;
     for (i = 0; i <= length; i++) {
-	b[i] = 0x69;
+	dest[i] = (unsigned char) val;
     }
 }
--- Theorems/Buf_Of.thy
+++ Theorems/Buf_Of.thy
@@ -4,13 +4,24 @@ begin
 
 install_C_file "../Files/buf_of.c"
 autocorres [
-    no_heap_abs = fill_buf,
-    unsigned_word_abs = fill_buf
+    no_heap_abs = fill_buf write_char,
+    unsigned_word_abs = fill_buf write_char
 ] "../Files/buf_of.c"
 
 context buf_of begin
 
 abbreviation "deref s x \<equiv> h_val (hrs_mem (t_hrs_' s)) x"
+
+lemma write_char_overflow_check:
+  "\<lbrace> \<lambda>s. c_guard(x::8 word ptr)
+         \<and> sz = size_of TYPE(8 word)
+         \<and> nat i > 0 
+         \<and> y = (ptr_add (ptr_add x (of_nat sz)) i)
+         \<and> P (deref s y) \<rbrace>
+     write_char' (ptr_coerce x) c
+    \<lbrace> \<lambda> _ s. P (deref s y) \<rbrace>"
+    oops
+    
 
 lemma buf_of_overflow_right:
   "\<lbrace> \<lambda>s. c_guard(x::8 word ptr)
@@ -18,9 +29,9 @@ lemma buf_of_overflow_right:
          \<and> nat i > 0 
          \<and> y = (ptr_add (ptr_add x (of_nat sz)) i)
          \<and> P (deref s y) \<rbrace>
-     fill_buf' (ptr_coerce x) sz
+     fill_buf' (ptr_coerce x) sz c
     \<lbrace> \<lambda> _ s. P (deref s y) \<rbrace>"
-    
+    sorry
     
 
 
