28165ab98563e6b1a468a6e49c24a48583935f5b
Scott Constable <sdconsta@syr.edu>
2014-10-30 09:11:03 -0400

    working on state lemma

--- Theorems/Buf_Of.thy
+++ Theorems/Buf_Of.thy
@@ -3,7 +3,7 @@ imports AutoCorres
 begin
 
 install_C_file "../Files/buf_of.c"
-autocorres [ heap_abs_syntax
+autocorres [ 
 ] "../Files/buf_of.c"
 
 context buf_of begin
@@ -11,14 +11,24 @@ context buf_of begin
 thm write_char'_def
 value write_char'
 
-lemma write_char_overflow_check:
+lemma state: "y \<noteq> 0 \<Longrightarrow> ((heap_w8 s)(x := scast (of_int c))) (x +\<^sub>p y) = heap_w8 s (x +\<^sub>p y)"
+sorry
+
+theorem write_char_overflow_check:
   "\<lbrace> \<lambda>s. is_valid_w8 s x
          \<and> sz = size_of TYPE(8 word)
          \<and> y = ptr_add x (of_nat sz)
+         \<and> is_valid_w8 s y
          \<and> P (heap_w8 s y) \<rbrace>
      write_char' (ptr_coerce x) c
     \<lbrace> \<lambda> _ s. P (heap_w8 s y) \<rbrace>!"
-oops
+  apply (unfold write_char'_def)
+  apply clarsimp
+  apply wp 
+  apply clarsimp
+  apply (subst state)
+  apply auto
+done
     
 lemma buf_of_overflow_right:
   "\<lbrace> \<lambda>s. is_valid_w8 s x
