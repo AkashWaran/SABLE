0763ac7fed8cb937578c08685933572bdf5846e1
Scott Constable <sdconsta@syr.edu>
2014-10-29 21:48:03 -0400

    updated hoare triples to accomodate lifted heaps

--- Theorems/Buf_Of.thy
+++ Theorems/Buf_Of.thy
@@ -3,34 +3,31 @@ imports AutoCorres
 begin
 
 install_C_file "../Files/buf_of.c"
-autocorres [
-    no_heap_abs = fill_buf write_char,
-    unsigned_word_abs = fill_buf write_char
+autocorres [ heap_abs_syntax
 ] "../Files/buf_of.c"
 
 context buf_of begin
 
-abbreviation "deref s x \<equiv> h_val (hrs_mem (t_hrs_' s)) x"
+thm write_char'_def
+value write_char'
 
 lemma write_char_overflow_check:
-  "\<lbrace> \<lambda>s. c_guard(x::8 word ptr)
+  "\<lbrace> \<lambda>s. is_valid_w8 s x
          \<and> sz = size_of TYPE(8 word)
-         \<and> nat i > 0 
-         \<and> y = (ptr_add (ptr_add x (of_nat sz)) i)
-         \<and> P (deref s y) \<rbrace>
+         \<and> y = ptr_add x (of_nat sz)
+         \<and> P (heap_w8 s y) \<rbrace>
      write_char' (ptr_coerce x) c
-    \<lbrace> \<lambda> _ s. P (deref s y) \<rbrace>"
-    oops
+    \<lbrace> \<lambda> _ s. P (heap_w8 s y) \<rbrace>!"
+oops
     
-
 lemma buf_of_overflow_right:
-  "\<lbrace> \<lambda>s. c_guard(x::8 word ptr)
+  "\<lbrace> \<lambda>s. is_valid_w8 s x
          \<and> sz = size_of TYPE(8 word)
          \<and> nat i > 0 
          \<and> y = (ptr_add (ptr_add x (of_nat sz)) i)
-         \<and> P (deref s y) \<rbrace>
-     fill_buf' (ptr_coerce x) sz c
-    \<lbrace> \<lambda> _ s. P (deref s y) \<rbrace>"
+         \<and> P (heap_w8 s y) \<rbrace>
+     fill_buf' (ptr_coerce x) (of_nat sz) c
+    \<lbrace> \<lambda> _ s. P (heap_w8 s y) \<rbrace>!"
     sorry
     
 
