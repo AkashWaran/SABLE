ad45f42ea825a153fbc79a7b333717b3f6b87797
Akash <akash.waran@gmail.com>
2014-10-31 15:11:54 -0400

    Proving buffer overflow for single character

--- Theorems/Buf_Of.thy
+++ Theorems/Buf_Of.thy
@@ -10,12 +10,45 @@ context buf_of begin
 
 thm write_char'_def
 value write_char'
+  
+lemma ptrEq: "Ptr (ptr_val x) = x"
+  apply auto
+  done  
+  
+lemma of_int_i_impl: "of_int i = 0 \<Longrightarrow> i = 0"
+  apply (rule )
+  apply (rule of_int_eq_0_iff)
+  
+  
+  
+  sorry
 
+lemma ptrAdd: "i \<noteq> 0 \<Longrightarrow> ptr_val x + of_int i \<noteq> ptr_val x"
+  apply auto;
+  apply (drule of_int_i_impl)
+  apply auto
+  done
+  
+lemma ptrEqOnAdd: "i \<noteq> 0 \<Longrightarrow> Ptr (ptr_val x + of_int i) \<noteq> x"
+  apply auto
+  apply (drule ptrAdd ptrEq)
+  apply auto
+  by (metis buf_of.ptrAdd of_int_0 ptr_val.ptr_val_def)
+  
 lemma noteq: "i \<noteq> 0 \<Longrightarrow> (x :: 8 word ptr) +\<^sub>p i \<noteq> x"
   apply (clarsimp simp: ptr_add_def)
-  
-sorry
-
+  apply (subgoal_tac "i > 0")
+    apply (case_tac "i = 1")
+      apply simp
+      apply (metis add.commute add_right_cancel monoid_add_class.add.left_neutral ptr_val.ptr_val_def zero_neq_one)
+    apply (subgoal_tac "i < 0")
+    apply (case_tac "i = -1")   
+      apply simp
+      apply (metis linorder_not_le zless_add1_eq zless_imp_add1_zle)
+    apply (drule ptrEqOnAdd)
+    apply auto
+    by (metis buf_of.ptrEqOnAdd)
+      
 lemma state: "i \<noteq> 0 \<Longrightarrow> ((heap_w8 s)(x := scast (of_int c))) (x +\<^sub>p i) = heap_w8 s (x +\<^sub>p i)"
   apply (simp only: fun_upd_apply)
   apply (drule noteq)
